aef3107cba629303d5c0c43fc1bc1c9c6fa92e55
Tim Zimmermann <tim@linux4.de>
2020-05-03 11:13:02 +0200

    msm8917-common: use lineage charger
    Animation from: https://github.com/PixelExperience/vendor_pixelstyle/tree/ten/charger
    
    Signed-off-by: geckyn <geckyn@gmail.com>

--- msm8917.mk
+++ msm8917.mk
@@ -115,6 +115,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libsuspend
 
+# Charger animation
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/charger/animation.txt:$(TARGET_COPY_OUT_PRODUCT)/etc/res/values/charger/animation.txt \
+    $(LOCAL_PATH)/configs/charger/battery_scale.png:$(TARGET_COPY_OUT_PRODUCT)/etc/res/images/charger/battery_scale.png \
+    $(LOCAL_PATH)/configs/charger/battery_fail.png:$(TARGET_COPY_OUT_PRODUCT)/etc/res/images/charger/battery_fail.png \
+    $(LOCAL_PATH)/configs/charger/main_font.png:$(TARGET_COPY_OUT_PRODUCT)/etc/res/images/charger/main_font.png
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/camera/A13QL_s5k3l6xx_module_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/A13QL_s5k3l6xx_module_info.xml \
     $(LOCAL_PATH)/camera/B13QL_s5k3l6xx_module_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/B13QL_s5k3l6xx_module_info.xml \
