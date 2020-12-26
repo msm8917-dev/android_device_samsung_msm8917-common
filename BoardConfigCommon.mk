be38b70cdac8fcacf8d0efb094de86948db5b84e
Ataberk Özen <ataberkozen123@gmail.com>
2020-12-26 15:12:47 +0000

    msm8917-common: correct sepolicy inclusion
    Signed-off-by: Ataberk Özen <ataberkozen123@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -172,7 +172,7 @@ BOARD_ROOT_EXTRA_FOLDERS := config omr efs
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 SELINUX_IGNORE_NEVERALLOWS := true
 
