6c1f3e372910104e556f559c90bbe71c47643caa
Linux4 <tim@linux4.de>
2022-11-09 15:00:17 +0000

    msm8917-common: camera/provider: Add SamsungCameraProvider

--- msm8917.mk
+++ msm8917.mk
@@ -108,7 +108,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.5-service \
+    android.hardware.camera.provider@2.5-service.msm8917 \
     Snap
 
 PRODUCT_COPY_FILES += \
