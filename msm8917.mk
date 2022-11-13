349ffa8356ca851b03ac90b5780c69e2d3e63af2
roynatech2544 <whiteshell2544@naver.com>
2022-11-13 19:44:39 +0900

    msm8917-common: Changes to DRM
    - widevine hal is nowhere avaliable on AOSP so its dead target
    - Uprev clearkey to 1.4
    - Remove clearkey entry from vintf as it has sperate fragment
    - Remove android.hardware.drm@1.4.vendor as 1.4 intf lib is implicitly selected by drm 1.4 clearkey

--- msm8917.mk
+++ msm8917.mk
@@ -185,8 +185,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.1-service.widevine \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.4-service.clearkey
     
 # Ebtables
 PRODUCT_PACKAGES += \
@@ -225,7 +224,6 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.0.vendor \
     android.hardware.radio.deprecated@1.0 \
     android.hardware.radio.deprecated@1.0.vendor \
-    android.hardware.drm@1.4.vendor \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
