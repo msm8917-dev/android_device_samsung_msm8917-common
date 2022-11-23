1950e3c60cfd1a207830615add8e31ef6e84fc77
roynatech2544 <whiteshell2544@naver.com>
2022-11-23 22:04:04 +0900

    msm8917-common: Support device-specific overlay

--- msm8917.mk
+++ msm8917.mk
@@ -15,7 +15,8 @@
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-$(OVERLAY_DEVICE)
     
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
