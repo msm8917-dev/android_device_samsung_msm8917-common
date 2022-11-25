73baf59bcac86f0b4274828aba2b74b113b1dd2a
roynatech2544 <whiteshell2544@naver.com>
2022-11-25 12:49:33 +0900

    msm8917-common: Uprev audio hals and put only needed to list

--- msm8917.mk
+++ msm8917.mk
@@ -47,15 +47,9 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio@2.0-service \
-    android.hardware.audio@4.0 \
-    android.hardware.audio@4.0-impl \
-    android.hardware.audio.common@4.0 \
-    android.hardware.audio.common@4.0-util \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@4.0 \
-    android.hardware.audio.effect@4.0-impl
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.service \
+    android.hardware.audio.effect@6.0-impl
 
 # Bluetooth
 PRODUCT_PACKAGES += \
