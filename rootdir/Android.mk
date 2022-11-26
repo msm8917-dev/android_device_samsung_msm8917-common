b3381b7934a3a39c0f2aa0f731205c78e5a6953f
roynatech2544 <whiteshell2544@naver.com>
2022-11-26 13:33:36 +0900

    msm8917-common: Add modprobe.rc

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,5 +1,4 @@
 LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.class_late.sh
@@ -190,6 +189,14 @@ LOCAL_MODULE       := init.wifi.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.wifi.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := modprobe.vendor.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/modprobe.vendor.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
