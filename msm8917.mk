2f779dbdc49fbe80a9594ee89b0995dd0744332b
roynatech2544 <whiteshell2544@naver.com>
2022-12-02 01:34:16 +0000

    msm8917-common: Drop unused targets

--- msm8917.mk
+++ msm8917.mk
@@ -466,8 +466,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
-    vendor.qti.hardware.wifi.hostapd@1.0.vendor \
-    vendor.qti.hardware.wifi.supplicant@2.0.vendor \
     WifiOverlay
 
 PRODUCT_COPY_FILES += \
